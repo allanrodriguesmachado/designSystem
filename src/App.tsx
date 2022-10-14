1c2dac2f080c6a23a6bb6975a7bbb9b8c2a10751
38672505883 <allan.shibata@gmail.com.br>
2022-10-14 13:36:38 -0300

    Visual structure design system

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './styles/global.css';
+
+export function App() {
+    return (
+        <>
+            <h1 className="font-bold text-2xl text-indigo-500">Hello World</h1>
+
+            <button className="bg-violet-500 text-white font-medium px-8 rounded hover:bg-violet-600">
+                Enviar
+            </button>
+        </>
+    )
+}
+
