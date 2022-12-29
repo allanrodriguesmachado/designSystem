89f5daafed574ed98408496f6317bbfec7c33d16
38672505883 <allan.shibata@gmail.com.br>
2022-12-29 16:30:03 -0300

    :up:

--- .storybook/manager.js
+++
@@ -1,6 +0,0 @@
-import addons, { Addon } from "@storybook/addons";
-import { themes } from '@storybook/theming'
-
-addons.setConfig({
-    theme: themes.dark
-})
--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import './styles/global.css'
-export function App() {
-  return (
-    <>
-      <h1 className='font-bold text-5xl text-violet-500'>Hello World</h1>
-      <button className='bg-cyan-500 text-xxl px-4 py-3 rounded text-white hover:bg-violet-600'>Opaaa</button>
-    </>
-  )
-}
